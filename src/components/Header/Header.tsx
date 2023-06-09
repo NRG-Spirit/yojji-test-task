c9922c30fbfedb2b5419f42e7553bd5ddab9b0cc
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2023-06-10 01:26:12 +0300

    fix: links in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,13 @@
 import { AppBar } from '@mui/material';
-import Link from '@mui/material/Link';
+import { NavLink } from 'react-router-dom';
 import Box from '@mui/material/Box';
 
 export default function Header() {
   return (
     <AppBar position="static" color="primary">
       <Box sx={{ display: 'flex' }} m={2}>
-        <Link href="/" color="inherit" underline="none" mr={1}>
-          Home
-        </Link>
-        <Link href="/task" color="inherit" underline="none">
-          Task
-        </Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/task">Task</NavLink>
       </Box>
     </AppBar>
   );
